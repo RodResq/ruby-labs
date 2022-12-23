f5eeab209a75dd92f94b39cc0464b3b9084d7c16
RodResq <rodrigoresque@yahoo.com.br>
2022-12-23 16:30:08 -0300

    updt

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,6 +1,21 @@
 puts "Bem vindo ao jogo de advinhacao"
 puts "Qual e o seu nome?"
 nome = gets
+puts "\n\n\n\n"
+puts "Comeceramos o jogo para voce, " + nome
 
-puts "Comeceramos o jogo para voce, "
-puts nome
+puts "Escolhendo um numero secreto entre 0 e 200 ..."
+numero_secreto = 175
+puts "Escolhido... que tal adivinhar hoje nosso numero secreto?"
+puts "\n\n\n"
+puts "Tentativa 1"
+puts "Entre com o numero"
+chute = gets
+puts "Sera que acertou? Voce chutou " + chute
+puts chute.to_i == numero_secreto
+
+if numero_secreto == chute.to_i
+    puts "Voce acertou!"
+else
+    puts "Voce errou!"
+end
