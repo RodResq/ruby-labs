06337e08ae5f8a53db333b3cfec95495882bbf88
RodResq <rodrigoresque@yahoo.com.br>
2022-12-27 17:57:36 -0300

    updt

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,27 +1,32 @@
-puts "Bem vindo ao jogo de advinhacao"
-puts "Qual e o seu nome?"
-nome = gets
-puts "\n\n\n\n"
-puts "Comeceramos o jogo para voce, " + nome
+def da_boas_vindas
+    puts "Bem vindo ao jogo de advinhacao"
+    puts "Qual e o seu nome?"
+    nome = gets
+    puts "\n\n\n\n"
+    puts "Comeceramos o jogo para voce, " + nome
+end
 
-puts "Escolhendo um numero secreto entre 0 e 200 ..."
-numero_secreto = 175
-puts "Escolhido... que tal adivinhar hoje nosso numero secreto?"
+def sorteia_numero_secreto
+    puts "Escolhendo um numero secreto entre 0 e 200 ..."
+    sorteado = 175
+    puts "Escolhido... que tal adivinhar hoje nosso numero secreto?"
+    sorteado
+end
 
-limite_de_tentativas = 5
-
-for tentativa in 1..limite_de_tentativas do
+def pede_um_numero(tentativa, limite_de_tentativas)
     puts "\n\n\n"
     puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
     puts "Entre com o numero"
     chute = gets
     puts "Sera que acertou? Voce chutou " + chute
-    puts chute.to_i == numero_secreto
+    chute.to_i
+end
 
-    acertou = numero_secreto == chute.to_i
+def varifica_se_acertou(numero_secreto, chute)
+    acertou = numero_secreto == chute
     if acertou
         puts "Voce acertou!"
-        break
+        return true
     else
         maior = numero_secreto > chute.to_i
         if maior 
@@ -30,4 +35,15 @@ for tentativa in 1..limite_de_tentativas do
             puts("O Numero secreto é menor!")
         end
     end
+    false
+end
+
+da_boas_vindas
+numero_secreto = sorteia_numero_secreto
+
+limite_de_tentativas = 5
+
+for tentativa in 1..limite_de_tentativas do
+    chute = pede_um_numero tentativa, limite_de_tentativas 
+    break if varifica_se_acertou numero_secreto, chute 
 end
