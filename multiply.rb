a524ed33c153563efcdef139719238af9d88aac5
RodResq <rodrigoresque@yahoo.com.br>
2023-01-06 17:46:22 -0300

    updt

---
+++ multiply.rb
@@ -0,0 +1,8 @@
+def multiply(val1, val2)
+    result = val1 * val2
+    puts result
+end
+
+if __FILE__ == $0
+    multiply(2, 10)
+end
---
+++ saysomething.rb
@@ -0,0 +1,8 @@
+def saysomething()
+    puts "Hello"
+end
+
+
+if __FILE__ == $0
+    saysomething
+end
