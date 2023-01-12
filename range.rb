4c9705b7dc3f6d23eb0095cdc58616a9b19e0ed4
RodResq <rodrigoresque@yahoo.com.br>
2023-01-12 17:18:36 -0300

    updt

--- range.rb
+++ range.rb
@@ -1,5 +1,9 @@
-words = 'cal'..'car'
-puts words.min
-puts words.max
-words.reject {|subrange| puts subrange < 'cal'}
-words.each {|word| puts "Hello " + word}
+# words = 'cal'..'car'
+# puts words.min
+# puts words.max
+# words.reject {|subrange| puts subrange < 'cal'}
+# words.each {|word| puts "Hello " + word}
+
+while input = gets
+    puts input + " triggered" if input =~ /start/ .. input =~ /end/
+end
