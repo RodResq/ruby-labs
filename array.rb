bb11dfab3e1b8e0abdaa621df65eed1dd3840c10
RodResq <rodrigoresque@yahoo.com.br>
2023-01-12 18:08:09 -0300

    updt

---
+++ array.rb
@@ -0,0 +1,10 @@
+days_of_week = Array.new
+puts days_of_week.empty?
+
+days_of_week = Array.new(7, "today")
+puts days_of_week
+
+days_of_week = Array["Mon", "Tues", "Tues", "Thu", "Fri", "Sat", "Sum"]
+puts days_of_week
+
+days_of_week = ["Mon", "Tues", "Wed", "Thu", "Fri", "Sat", "Sun"]
